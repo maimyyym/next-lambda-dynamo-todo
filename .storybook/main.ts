ba30d8fb179e271313a564b0774fe1d1b4d833c0
maimy <m.711029@gmail.com>
2023-04-28 14:27:48 +0900

    ディレクトリ・ファイル分割

---
+++ .storybook/main.ts
@@ -0,0 +1,46 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+const config: StorybookConfig = {
+    stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+    ],
+    framework: {
+        name: '@storybook/nextjs',
+        options: {},
+    },
+    docs: {
+        autodocs: 'tag',
+    },
+};
+export default config;
+
+module.exports = {
+  stories: ['../src/**/*.stories.@(tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-controls',
+  ],
+  webpackFinal: async (config) => {
+    // Tailwind CSSの設定
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              ident: 'postcss',
+              plugins: [require('tailwindcss'), require('autoprefixer')],
+            },
+          },
+        },
+      ],
+      include: require('path').resolve(__dirname, '../'),
+    });
+    return config;
+  },
+};
+
